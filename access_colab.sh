da55efdb6f11b4fc2d77728b1b5f040f9bbec3cf
upura <upura0@gmail.com>
2020-06-23 09:27:39 +0900

    update to run in 24 hours

--- access_colab.sh
+++ access_colab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in `seq 0 12`
+for i in `seq 0 24`
 do
   echo "[$i]" ` date '+%y/%m/%d %H:%M:%S'` "connected."
-  open https://colab.research.google.com/drive/16FxkNDXis5dDkJA0RHZu9x9lyxkY-y3s?authuser=1
+  open https://colab.research.google.com/drive/1EUU9jQj8ZZTXo1UyesyJGs-0-p8Zc_nC?authuser=1
   sleep 3600
 done
